b4b130e0e98423e6ca6801ef3b26da6be0607765
StefanoSalsano <stefano.salsano@uniroma2.it>
2015-11-06 03:19:40 +0100

    service_start.sh minor changes

--- service_start.sh
+++ service_start.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 
-
-
 if [ $1 ]; then
 
-
-
 	echo -e "\n"
-
 	echo "#################################################"
-
 	echo "##     DREAMER IP/SDN service Launcher         ##"
-
 	echo "##                                             ##"
-
-	echo "##  The process can last many minutes. Please  ##"
-
-	echo "##  wait and do not interrupt the process.     ##"
-
+	echo "##    The process can last some minutes.       ##"
+	echo "##          Please be patient :-)              ##"
 	echo "#################################################"
 
-
-
-	cp workspace/Dreamer-Mininet-Extensions/vll_pusher.cfg workspace/Dreamer-VLL-Pusher/ryu/
-
-	cd workspace/Dreamer-VLL-Pusher/ryu/
-
+	cp /home/user/workspace/Dreamer-Mininet-Extensions/vll_pusher.cfg /home/user/workspace/Dreamer-VLL-Pusher/ryu/
+	cd /home/user/workspace/Dreamer-VLL-Pusher/ryu/
 	./vll_pusher.py --controller $1:8080 --add
-
-
 
 else
 
 	echo "Usage: specify controller ip!!"
-
-
 
 fi
 
