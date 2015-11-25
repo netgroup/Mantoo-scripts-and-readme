8688a17e5b6859ddbc12f110e5ea05430b143031
StefanoSalsano <stefano.salsano@uniroma2.it>
2015-11-25 01:52:57 +0100

    update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -7,18 +7,43 @@ if [ "$1" = "my_workspace" ];then
 fi	
 
 REPOS[0]=$REPO_PATH/Dreamer-Experiment-Handler
+#https://github.com/netgroup/Dreamer-Experiment-Handler.git
+
 REPOS[1]=$REPO_PATH/Dreamer-Management-Scripts
+#https://github.com/netgroup/Dreamer-Management-Scripts.git
+
 REPOS[2]=$REPO_PATH/Dreamer-Measurements-Tools
+#https://github.com/netgroup/Dreamer-Measurements-Tools.git
+
 REPOS[3]=$REPO_PATH/Dreamer-Mininet-Extensions
+#https://github.com/netgroup/Dreamer-Mininet-Extensions.git
+
 REPOS[4]=$REPO_PATH/dreamer-ryu
+#https://github.com/netgroup/dreamer-ryu.git
+
 REPOS[5]=$REPO_PATH/Dreamer-Topology3D
+#https://github.com/netgroup/Dreamer-Topology3D.git
+
 REPOS[6]=$REPO_PATH/Dreamer-Topology-and-Service-Validator
+#https://github.com/netgroup/Dreamer-Topology-and-Service-Validator.git
+
 REPOS[7]=$REPO_PATH/Dreamer-Topology-Parser-and-Validator
+#https://github.com/netgroup/Dreamer-Topology-Parser.git
+
 REPOS[8]=$REPO_PATH/Dreamer-VLL-Pusher
+#https://github.com/netgroup/Dreamer-VLL-Pusher.git
+
 REPOS[9]=$REPO_PATH/floodlight-0.90
+#https://github.com/netgroup/floodlight.git
+
 REPOS[10]=$REPO_PATH/Mantoo-scripts-and-readme
+#https://github.com/netgroup/Mantoo-scripts-and-readme.git
+
 REPOS[11]=$REPO_PATH/OSHI-monitoring
+#https://github.com/netgroup/OSHI-monitoring.git
+
 REPOS[12]=$REPO_PATH/OSHI-REST-server
+#https://github.com/netgroup/OSHI-REST-server.git
 
 
 printandexec () {
