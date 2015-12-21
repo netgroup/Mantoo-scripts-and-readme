e1d4c0644e324fe0a47e4a6d5f3fe32544c62958
giorgio <marini_giorgio@live.it>
2015-12-21 09:39:56 +0100

    added script for monitoring optional

---
+++ ryu_startMod.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+ 	echo "#################################################"
+	echo "##     OSHI Hybrid IP/SDN Hybrid ryu_start     ##"
+	echo "##                                             ##"
+	echo "##  The process can last many minutes. Please  ##"
+	echo "##  wait and do not interrupt the process.     ##"
+	echo "#################################################"
+
+	WORKSPACE="workspace"
+	cd /home/user/$WORKSPACE/dreamer-ryu/ryu/app
+	ryu-manager --verbose --observe-links ofctl_rest.py rest_topology
+fi
+
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+    -m|--mode)
+    MODE="$2"
+    shift # past argument
+    ;;
+esac
+shift
+done
+if [[ ${MODE} = 'monitoring' ]]
+	then
+		echo "Setting up environment..."
+	  	apt-get install rrdtool librrds-perl librrd-dev sshpass
+	  	pip install rrdtool
+
+		echo "#################################################"
+		echo "##     OSHI Hybrid IP/SDN Hybrid ryu_start     ##"
+		echo "##        with a monitoring processing   	     ##"
+		echo "##  The process can last many minutes. Please  ##"
+		echo "##  wait and do not interrupt the process.     ##"
+		echo "#################################################"
+
+		WORKSPACE="workspace"
+		cd /home/user/$WORKSPACE/dreamer-ryu/ryu/app
+		ryu-manager --verbose --observe-links ofctl_rest.py rest_topology traffic_monitor.py
+else 
+          echo "Unrecognized option. Available options are: setup, runmininet and runryu. This script will now terminate."
+          exit 1
+fi
