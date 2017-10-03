47ca8559965e54aec725d0d20166acaad7a3e33d
Stefano Salsano <stefano.salsano@uniroma2.it>
2017-10-03 15:18:50 +0200

    msg

---
+++ initial_install-v3.sh
@@ -0,0 +1,350 @@
+#!/bin/bash
+
+MYUSER="user"
+HOME_DIR="/home/$MYUSER"
+WORKSPACE_DIR="$HOME_DIR/workspace"
+MININET_DIR="$HOME_DIR/mininet"
+
+# Debug banner
+echo -e "\n"
+echo "#############################################################"
+echo "##                OSHI VM Setup Tools                      ##"
+echo "##                                                         ##"
+echo "##       The process can last many minutes. Please         ##"
+echo "##       do not interrupt the process.                     ##"
+echo "#############################################################"
+echo -e "\n"
+
+# disable apt-daily.service if it is running
+sudo systemctl stop apt-daily.service
+sudo systemctl kill --kill-who=all apt-daily.service
+# wait until `apt-get updated` has been killed
+while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
+do
+  sleep 1;
+done
+
+
+# Update apt
+sudo apt-get update
+# Install openssh dependency
+sudo apt-get install -y openssh-server
+# Install tcpdump
+sudo apt-get install -y tcpdump
+# Install iproute
+sudo apt-get install -y iproute
+# Install python-pip
+sudo apt-get install -y python-pip
+# Install virtualenv
+sudo apt-get install -y virtualenv
+# Install ssh-pass
+sudo apt-get install -y sshpass
+# Install wondershaper
+sudo apt-get install -y wondershaper
+# Install git-core
+sudo apt-get install -y git-core
+# Install net-tools
+sudo apt-get install -y net-tools
+# Install bison
+sudo apt-get install -y bison
+# Install flex
+sudo apt-get install -y flex
+# Install traceroute
+sudo apt-get install -y traceroute
+
+# Install sublime evaluation version
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
+
+#################################
+#adding packages from Mantoo/OSHI
+
+echo -e "\n-Installing VIM"
+sudo apt-get install -y vim &&
+
+echo -e "\n-Installing python-simplejson"
+sudo apt-get install -y python-simplejson &&
+
+echo -e "\n-Installing Python-QT4"
+sudo apt-get install -y python-qt4 &&
+
+echo -e "\n-Installing Python Zope Interface"
+sudo apt-get install -y python-zopeinterface &&
+
+echo -e "\n-Installing Python-Twisted-Conch"
+sudo apt-get install -y python-twisted-conch &&
+
+echo -e "\n-Installing pkg-config"
+sudo apt-get install -y pkg-config &&
+
+echo -e "\n-Installing dh-autoreconf"
+sudo apt-get install -y dh-autoreconf &&
+
+echo -e "\n-Installing ipcalc"
+sudo apt-get install -y ipcalc &&
+
+echo -e "\n-Installing Linux Headers for Linux kernel `uname -r`"
+sudo apt-get install -y linux-headers-`uname -r` &&
+
+echo -e "\n-Installing OpenVPN"
+sudo apt-get install -y openvpn &&
+
+echo -e "\n-Installing VLAN packages"
+sudo apt-get install -y vlan &&
+
+echo -e "\n-Installing Quagga router services"
+sudo apt-get install -y gawk
+sudo apt-get install -y texinfo
+
+#sudo apt-get install -y quagga
+wget https://launchpad.net/~ubuntu-security-proposed/+archive/ubuntu/ppa/+build/11037964/+files/quagga_0.99.24.1-2ubuntu1.2_amd64.deb
+sudo apt-get install libreadline6
+sudo touch /etc/quagga/zebra.conf
+sudo touch /etc/quagga/ospfd.conf
+
+echo -e "\n-Upgrading Quagga router services"
+
+# Download quagga with the support for fpm
+wget http://download.savannah.gnu.org/releases/quagga/quagga-0.99.24.1.tar.gz -O /home/user/quagga.tar.gz
+cd /home/user
+
+tar -xzvf quagga.tar.gz
+cd quagga-0.99.24.1/
+
+# Run configure
+./configure --enable-user=quagga --enable-group=quagga --enable-vty-group=quagga --enable-fpm --sysconfdir=/etc/quagga --localstatedir=/var/run/quagga --libdir=/usr/lib/quagga/
+make
+sudo make install
+sudo ldconfig
+
+# Generate commands and substitute old commands with new ones
+cd zebra/
+sudo ./zebra &
+sleep 1
+sudo killall lt-zebra 2> /dev/null
+sudo killall zebra 2> /dev/null
+cd .libs/
+sudo cp zebra /usr/lib/quagga/zebra
+cd ../../
+cd ospfd/
+sudo ./ospfd &
+sleep 1
+sudo killall lt-ospfd 2> /dev/null
+sudo killall ospfd 2> /dev/null
+cd .libs/
+sudo cp ospfd /usr/lib/quagga/ospfd
+
+
+echo -e "-VLAN module setup"
+sudo modprobe 8021q &&
+# Make 801q module loading permanent
+if [ $(sudo cat /etc/modules | grep 8021q | wc -l) -eq 0 ]
+	then
+		sudo echo "8021q" >> /etc/modules
+fi
+
+
+#echo -e "\n-Installing iproute"
+#apt-get install -y iproute
+
+echo -e "\n-Installing sudo"
+sudo apt-get install -y sudo
+
+##########################################
+
+
+# Install IPRoute2
+wget https://git.kernel.org/pub/scm/linux/kernel/git/shemminger/iproute2.git/snapshot/iproute2-4.13.0.tar.gz
+# Extract the content
+tar -xzvf iproute2-4.13.0.tar.gz
+# Enter in the source folder
+cd iproute2-4.13.0
+# Compile everything
+make
+# Install new iproute2
+sudo make install
+
+
+mkdir $MININET_DIR
+#chown $MYUSER:$MYUSER $MININET_DIR
+cd $MININET_DIR
+
+# Install Mininet
+git clone git://github.com/mininet/mininet
+# Run "ALL" setup
+
+mininet/util/install.sh -a
+
+#virtualenv env
+#source env/bin/activate
+
+# Python dependencies
+pip install ipaddress
+# Networkx
+pip install networkx==1.11
+# netaddr (tool for subnetting)
+pip install netaddr
+
+
+#deactivate
+
+sudo /etc/init.d/openvswitch-switch stop
+sudo /etc/init.d/openvswitch-testcontroller stop
+sudo apt-get remove openvswitch-switch
+sudo apt-get remove openvswitch-testcontroller
+
+
+echo -e "\n-Installing OpenVSwitch"
+# Creating folder for OVS under /opt/ovs
+rm -f -r /opt/ovs 2> /dev/null
+mkdir -p /opt/ovs
+# Remove previous kernel module
+modprobe -r openvswitch 2> /dev/null
+mv /lib/modules/`uname -r`/kernel/openvswitch/openvswitch.ko /lib/modules/`uname -r`/kernel/openvswitch/openvswitch.ko.orig 2> /dev/null
+# Downloading
+cd /opt/ 
+git clone https://github.com/openvswitch/ovs.git
+cd  /opt/ovs/ 
+# Boot up and configuring sources
+./boot.sh &&
+./configure --with-linux=/lib/modules/`uname -r`/build &&
+# Make
+make &&
+make install &&
+# OVS module installation
+make modules_install &&
+mkdir -p /lib/modules/`uname -r`/kernel/openvswitch
+cp /opt/ovs/datapath/linux/openvswitch.ko /lib/modules/`uname -r`/kernel/openvswitch/openvswitch.ko
+depmod -a &&
+# Making module loading permanent
+modprobe openvswitch &&
+if [ $(cat /etc/modules | grep openvswitch | wc -l) -eq 0 ]
+	then
+		echo "openvswitch" >> /etc/modules
+fi
+# Create and initialize the database
+rm -f -r /usr/local/etc/openvswitch/ 2> /dev/null
+mkdir -p /usr/local/etc/openvswitch &&
+rm -f -r /usr/local/var/run/openvswitch/ 2> /dev/null
+mkdir -p /usr/local/var/run/openvswitch &&
+ovsdb-tool create /usr/local/etc/openvswitch/conf.db /opt/ovs/vswitchd/vswitch.ovsschema &&
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                     --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                     --private-key=db:Open_vSwitch,SSL,private_key \
+                     --certificate=db:Open_vSwitch,SSL,certificate \
+                     --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+                     --pidfile --detach
+ovs-vsctl --no-wait init &&
+# Starting OVS
+echo -e "\n-Starting OpenVSwitch"
+ovs-vswitchd --pidfile --detach &&
+# Adding OVS as a service
+echo -e "\n-Adding OpenVSwitch service"
+echo -e '#!/bin/bash
+#
+# start/stop openvswitch
+### BEGIN INIT INFO
+# Provides: openvswitchd
+# Required-start: $remote_fs $syslog
+# Required-stop: $remote_fs $syslog
+# Default-start: 2 3 4 5
+# Default-stop: 0 1 6
+# Short-description: OpenVSwitch daemon
+# chkconfig: 2345 9 99
+# description: Activates/Deactivates all Open vSwitch to start at boot time.
+# processname: openvswitchd
+# config: /usr/local/etc/openvswitch/conf.db
+# pidfile: /usr/local/var/run/openvswitch/ovs-vswitchd.pid
+### END INIT INFO\n
+
+PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+export PATH\n
+
+# Source function library. . /etc/rc.d/init.d/functions
+. /lib/lsb/init-functions\n
+
+stop()
+{
+echo "
+Stopping openvswitch..."\n
+
+if [ -e /usr/local/var/run/openvswitch/ovs-vswitchd.pid ]; then
+pid=$(cat /usr/local/var/run/openvswitch/ovs-vswitchd.pid)
+/usr/local/bin/ovs-appctl -t /usr/local/var/run/openvswitch/ovs-vswitchd.$pid.ctl exit
+rm -f /usr/local/var/run/openvswitch/ovs-vswitchd.$pid.ctl
+fi\n
+
+if [ -e /usr/local/var/run/openvswitch/ovsdb-server.pid ]; then
+pid=$(cat /usr/local/var/run/openvswitch/ovsdb-server.pid)
+/usr/local/bin/ovs-appctl -t /usr/local/var/run/openvswitch/ovsdb-server.$pid.ctl exit
+rm -f /usr/local/var/run/openvswitch/ovsdb-server.$pid.ctl
+fi\n
+
+rm -f /var/lock/subsys/openvswitchd
+echo "OK"
+}\n
+
+start()
+{
+echo "
+Starting openvswitch..."
+/usr/local/sbin/ovsdb-server /usr/local/etc/openvswitch/conf.db \
+--remote=punix:/usr/local/var/run/openvswitch/db.sock \
+--remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+--private-key=db:Open_vSwitch,SSL,private_key \
+--certificate=db:Open_vSwitch,SSL,certificate \
+--bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+--pidfile --detach\n
+
+/usr/local/bin/ovs-vsctl --no-wait init
+/usr/local/sbin/ovs-vswitchd unix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach\n
+
+mkdir -p /var/lock/subsys
+touch /var/lock/subsys/openvswitchd
+echo "
+OpenVSwitch started succesfully!"
+}\n
+
+# See how we were called.
+case $1 in
+start)
+start
+;;
+stop)
+stop
+;;
+restart)
+stop
+start
+;;
+status)
+status ovs-vswitchd
+;;
+*)
+echo "Usage: openvswitchd {start|stop|status|restart}."
+exit 1
+;;
+esac\n
+exit 0' > /etc/init.d/openvswitchd &&
+chmod +x /etc/init.d/openvswitchd &&
+update-rc.d openvswitchd defaults 
+
+
+mkdir $WORKSPACE_DIR
+cd $WORKSPACE_DIR
+
+git clone https://github.com/netgroup/Mantoo-scripts-and-readme.git
+cd Mantoo-scripts-and-readme
+./update_all_body.sh clone_repos
+
+
+
+# Change password for root
+#echo 'root:12345' | chpasswd
+# Enabling root login
+#sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# SSH login fix. Otherwise user is kicked off after login
+#sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
