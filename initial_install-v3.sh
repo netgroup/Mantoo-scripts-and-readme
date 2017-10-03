ee6e6fb624c0eb17615ae1e9e7035b6964251cc7
Stefano Salsano <stefano.salsano@uniroma2.it>
2017-10-03 23:32:55 +0200

    Update initial_install-v3.sh

--- initial_install-v3.sh
+++ initial_install-v3.sh
@@ -250,7 +250,7 @@ sudo /etc/init.d/openvswitch-switch stop
 sudo /etc/init.d/openvswitch-testcontroller stop
 sudo apt-get remove -y openvswitch-switch
 sudo apt-get remove -y openvswitch-testcontroller
-sudo apt autoremove
+sudo apt autoremove -y
 
 
 # Creating folder for OVS under /opt/ovs
