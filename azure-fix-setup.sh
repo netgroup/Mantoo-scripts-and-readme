7783054871720a3b898644315372b6a316673f88
Ubuntu <user@mydomain.org>
2017-12-09 00:37:56 +0000

    azure-fix-setup fixing

--- azure-fix-setup.sh
+++ azure-fix-setup.sh
@@ -2,8 +2,22 @@
 
 echo "Current user: $USER"
 
+VARI=$(ls -l /home | grep user-folder | awk '{print $3}')
+
+echo "Owner of /home/user-folder: $VARI"
+
+if [ "$USER" == "user" ]
+then
+   echo "The current user cannot be user"
+   echo "EXITING WITH NO ACTION"
+   return
+fi
+ 
+
 cd /home
-sudo chown --from=1000:1000 -R $USER:$USER user-folder
-sudo mv user-folder $USER
+sudo chown --from=$VARI:$VARI -R $USER:$USER user-folder
+sudo rm  -rf /home/$USER
+sudo mv /home/user-folder /home/$USER
+sudo rm /home/user
 sudo ln -s /home/$USER /home/user
 sudo chown -h $USER:$USER /home/user
