ca0b22268b1258c64d9843570ea1bcb5053e6e15
Stefano Salsano <stefano.salsano@uniroma2.it>
2017-12-19 23:07:29 +0100

    Update initial_install-v4.sh

--- initial_install-v4.sh
+++ initial_install-v4.sh
@@ -482,6 +482,26 @@ sudo rm -f AUTHORS
 sudo rm -f ChangeLog 
 
 echo -e "\n\n#####################################"
+echo -e "\n-Re-installing dependencies to run Ryu as root"
+#this is needed in the OSHI use case when running Ryu as root from the controller in Mininet
+
+sudo -H pip install webob
+sudo -H pip install routes
+sudo -H pip install paramiko
+sudo -H pip install 'oslo.config<2.0.0'
+sudo -H pip install msgpack-python
+sudo -H pip install lxml
+sudo -H pip install 'eventlet==0.20'
+
+sudo -H pip install ipaddress
+# Networkx
+sudo -H pip install networkx==$NETWORKX_VERSION
+# netaddr (tool for subnetting)
+sudo -H pip install netaddr
+sudo -H pip install siphash
+
+
+echo -e "\n\n#####################################"
 echo -e "\n-Setting up admin password for RDCL 3D"
 
 cd $WORKSPACE_DIR
